406974e7644fb3e429d202c4e877319353a40320
batuhanaltog <batuhanaltog88@gmail.com>
2025-07-22 23:55:51 +0300

    serving static files

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const userRouter = require('./routes/userRoutes');
 
 // Middlewares
 app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
