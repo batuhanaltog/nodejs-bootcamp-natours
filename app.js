4bc57411aa2e8293437623d73b8a1610526b871b
batuhanaltog <batuhanaltog88@gmail.com>
2025-07-23 00:22:19 +0300

    environment variables

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // Middlewares
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 app.use((req, res, next) => {
